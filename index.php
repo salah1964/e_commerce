6b25747048f8ad2a1dbd533fcf7cd37ec3153bb1
salah1964 <56828733+salah1964@users.noreply.github.com>
2020-08-29 15:11:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+   <head>
+      <title> First Page</title>
+	  <meta charset="utf-8" />
+	  <link rel="stylesheet" href="css/bootstrap.min.css" />
+	  <script src="js/jquery-3.5.1.min.js"></script>
+	  <script src="js/bootstrap.min.js"></script>
+   </head>
+   <body>
+     <button class="btn btn-success" id="btn1">send</button>
+     <?php
+	   echo'السلام عليكم';
+	 ?>
+
+   </body>
+</html>
